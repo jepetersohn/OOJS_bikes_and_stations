e3b3aaf1807edaad2a0d3ef6f1ca0814a699e8e8
Sarah Dudek <email": null>
2017-02-28 17:00:08 -0600

    Finished

--- spec/station_spec.js
+++ spec/station_spec.js
@@ -2,10 +2,11 @@ describe("a bike station", function() {
   var station, newerBike, olderBike, bikes
 
   beforeEach(function() {
-    newerBike = new Bike("12345", "red", 2016);
-    olderBike = new Bike("67890", "blue", 2015);
+    newerBike = new Bike("12345", "red", 2016, true, true, true);
+    olderBike = new Bike("67890", "blue", 2015, false, false, false);
+    someBike = new Bike("54634", "green", 1994, true, true, true)
 
-    station = new Station("Divvy", "Wood & Division", [newerBike, olderBike])
+    station = new Station("Divvy", "Wood & Division", [newerBike, olderBike], 2)
   });
 
   it("has a name", function() {
@@ -18,6 +19,10 @@ describe("a bike station", function() {
 
   it("has bikes", function() {
     expect(station.bikes).toEqual([newerBike, olderBike]);
+  });
+
+  it("has a capactity", function() {
+    expect(station.capacity).toEqual(2);
   });
 
   it("is empty if it has no bikes", function() {
@@ -36,12 +41,22 @@ describe("a bike station", function() {
       expect(station.bikes).toEqual([newerBike]);
     });
 
+    it("only takes capacity # of bikes", function() {
+      station.bikes =[newerBike, olderBike];
+      expect(function() {station.take();}).toThrowError(Error, "Can't add bikes to a full station!");
+    });
+
     it("takes multiple bikes at the same time", function() {
       bike = { id: "4567", color: "red", modelYear: 2010 }
       station.bikes = [bike]
 
       station.takeMultiple([newerBike, olderBike]);
       expect(station.bikes).toEqual([bike, newerBike, olderBike]);
+    });
+
+    it("throws an error when the station is full", function() {
+      station.bikes =[newerBike, olderBike];
+      expect(function(){ station.takeMultiple(); }).toThrowError(Error, "Can't add bikes to a full station!");
     });
   });
 
@@ -62,5 +77,23 @@ describe("a bike station", function() {
         expect(station.release()).toBeUndefined();
       });
     });
+  });
+
+  describe("repairing bikes", function(){
+    it("pumps the tires", function(){
+      station.tuneUp();
+      expect(station.bikes[0].flatTire).toEqual(false);
+    });
+
+    it("repairs the chain", function(){
+      station.tuneUp();
+      expect(station.bikes[0].brokenChain).toEqual(false);
+    })
+
+    it("tunes the bell", function(){
+      station.tuneUp();
+      expect(station.bikes[0].dullBell).toEqual(false);
+    })
+
   });
 });
--- src/bike.js
+++ src/bike.js
@@ -1,1 +1,25 @@
-var Bike = function() {}
+var Bike = function(id, color, modelYear, flatTire, brokenChain, dullBell) {
+  this.id = id,
+  this.color = color,
+  this.modelYear = modelYear,
+  this.flatTire = flatTire,
+  this.brokenChain = brokenChain,
+  this.dullBell = dullBell
+}
+
+Bike.prototype.pumpTire = function(){
+  this.flatTire = false;
+}
+
+Bike.prototype.repairChain = function(){
+  this.brokenChain = false;
+}
+
+Bike.prototype.tuneBell = function(){
+  this.dullBell = false;
+}
+
+Bike.prototype.statusReport = function(){
+  return (this.flatTire || this.brokenChain || this.dullBell)
+}
+
